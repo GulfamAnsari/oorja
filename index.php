41544651468884fd0b10f5ceb8c18631f53ff82c
Gulfam <gulfam.ansari@elucidata.io>
2018-02-24 17:34:29 +0530

    add bootstrap and required css

--- index.php
+++ index.php
@@ -2,7 +2,19 @@
 <html lang="en">
 <?php include ($_SERVER['DOCUMENT_ROOT'].'/metaTag.php');?>
 <?php include ($_SERVER['DOCUMENT_ROOT'].'/links.php');?>
+<!-- start-smooth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+		<script type="text/javascript" src="js/easing.js"></script>
+		<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+		</script>
+<!-- //start-smoth-scrolling -->
 <body>
-    <p>oorja</p>
+	oorja
 </body>
 </html>
