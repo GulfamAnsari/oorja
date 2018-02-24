8dc92320a9e0fc82f836712a231719a14d4308d8
Gulfam <gulfam.ansari@elucidata.io>
2018-02-24 17:09:41 +0530

    add generaal structure

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include ($_SERVER['DOCUMENT_ROOT'].'/metaTag.php');?>
+<?php include ($_SERVER['DOCUMENT_ROOT'].'/links.php');?>
+<body>
+    <p>oorja</p>
+</body>
+</html>
---
+++ metaTag.php
@@ -0,0 +1,21 @@
+<title>Oorja - An institute of trust</title>
+<meta name="description" content="Ooorja"/>
+<meta name="keywords" content="Oorja">
+<link rel="canonical" href="http://oorja.com/" />
+<meta name="robots" content="noodp " />
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="distribution" content="global">
+<!--Social site metatag-->
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Oorja - An institute of trust" />
+<meta property="og:description" content="Ooorja" />
+<meta property="og:url" content="http://Ooorja.com"/>
+<meta property="og:site_name" content="DroidTechknow" />
+<meta property="og:image" content="http://Ooorja.comimages/logo.png" />
+<meta property="og:image:width" content="1920" />
+<meta property="og:image:height" content="1079" />
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:description" content="Ooorja" />
+<meta name="twitter:title" content="Oorja - An institute of trust" />
+<meta name="twitter:image" content="http://Ooorja.comimages/logo.png" />
